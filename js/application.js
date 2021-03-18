d9233cd8bff79ff6167a69edcaaec10a5264e6fb
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2021-03-18 23:28:49 +0100

    displayed data in a loop

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     //need NMA API Key
     //https://www.nma.gov.au/about/our-collection/our-apis
@@ -8,25 +8,31 @@ $(document).ready(function(){
 
     var key = 'aGyOTzAchbsscZt7OnZQ4hSvgSsiW0hW'
 
+    var objectid = '71110'
+
     //api call 
     //api call is made to all the objects in the NMA of the Aboriginal Collection
-    var url = 'https://data.nma.gov.au/object/71110&apikey=' + key;
-    
-        $.getJSON(url, function(apiData) {
+    var url = 'https://data.nma.gov.au/object/' + objectid + '&apikey=' + key;
+    //api call is made to search the objects in the NMA that contain Aboriginal
+    var searchQuery = 'aboriginal';
+    var searchUrl = 'https://data.nma.gov.au/object?text=' + searchQuery;
+
+    $.getJSON(searchUrl, function (apiData) {
+        console.log('in search query');
         console.log(apiData);
-                    
-    $('.title').html (apiData.data[0].title);
-    $('.physdesc').html (apiData.data[0].physicalDescription);
-    $('.contributor').html (apiData.data[0].contributor[0].title);
-    $('.mediumtitle').html (apiData.data[0].medium[0].title);
-    $('.significanceStatement').html (apiData.data[0].significanceStatement);
 
-
+        // let's loop through the data
+        for (i = 0; i < apiData.data.length; i++) {
+            //variable to hold single record
+            var r = apiData.data[i]
+            // let's output the title to the console so we know it works.
+            console.log(r.title);
+            //showing the data as html
+            // create an item
+            var item = $('<div class="item">');
+            item.append('<h3>' + r.title + '</h3>');
+            item.append('<p>' + r.physicalDescription + '</p>');
+            $('.item-container').append(item);
+        }
+    });
 });
-
-});
-
-
-
-
-
