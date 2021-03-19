25da535dbf5cd7a826f30d9d61621dca12187b84
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2021-03-19 19:29:39 +0100

    images showing and subheadings

--- js/application.js
+++ js/application.js
@@ -1,49 +1,49 @@
-$(document).ready(function () {
-
-    //need NMA API Key
-    //https://www.nma.gov.au/about/our-collection/our-apis
-    //docs: https://github.com/NationalMuseumAustralia/Collection-API/wiki/Getting-started
-
-    //my API key
-
-    var key = 'aGyOTzAchbsscZt7OnZQ4hSvgSsiW0hW'
-
-    var objectid = '71110'
-
-    //api call 
-    //api call is made to all the objects in the NMA of the Aboriginal Collection
-    var url = 'https://data.nma.gov.au/object/' + objectid + '&apikey=' + key;
-    //api call is made to search the objects in the NMA that contain Aboriginal
-    var searchQuery = 'aboriginal';
-    var searchUrl = 'https://data.nma.gov.au/object?text=' + searchQuery;
-    // var searchfifity = 'object?text=aboriginal&offset=50' + searchQuery;
-
-    $.getJSON(searchUrl, function (apiData) {
-        console.log('in search query');
-        console.log(apiData);
-
-        // let's loop through the data
-        for (i = 0; i < apiData.data.length; i++) {
-            //variable to hold single record
-            var r = apiData.data[i]
-            // output the title to the console so we know it works.
-            console.log(r.title);
-
-            //showing the data as html
-            // create an item
-            var item = $('<div class="item">');
-            item.append('<h2>' + r.title + '</h2>');
-                      
-            item.append('<p>' + r.physicalDescription + '</p>');
-            item.append('<p>' + r.additionalType + '</p>');
-            item.append('<p>' + r.collection.title + '</p>');
-            if (r.significanceStatement != undefined) {
-                item.append('<p>' + r.significanceStatement + '</p>');
-            };
-            $('.item-container').append(item);
+function displayData(apiData) {
+    for (i = 0; i < apiData.data.length; i++) {
+        var r = apiData.data[i]
+        var item = $(`<div class="item">
+            <h2>${r.title}</h2>
+            <h3>Description</h3>
+                <p class="physdesc">${r.physicalDescription}</p>
+            <h3>Type</h3>
+            <p class="type">${r.additionalType}</p>
+            <h3>Collection</h3>
+            <p class="collection"> ${r.collection.title}</p>
+        </div>`);
+        if (r.significanceStatement != undefined) {
+            item.append('<h3>Significance Statement</h3> <p>' + r.significanceStatement + '</p>');
+        };
+        var thumbnail = extractThumbnail(r);
+        if (thumbnail) {
+            item.append('<img src="' + thumbnail + '" class="thumbnail">');
         }
-    });
+        $('.item-container').append(item);
+    }
+    if (apiData.links && apiData.links.next) {
+        loadDataFrom(apiData.links.next);
+    }
+}
+
+function extractThumbnail(row) {
+    if (row.hasVersion) {
+        var images = _.findWhere(row.hasVersion, { type: "StillImage" })
+        var thumbnail = _.findWhere(images.hasVersion, { version: "thumbnail image" })
+        return thumbnail.identifier;
+    }
+    return undefined;
+}
+
+function loadDataFrom(urlPath) {
+    var searchUrl = 'https://data.nma.gov.au/' + urlPath;
+    $.getJSON(searchUrl, displayData);
+}
+
+function loadDataForSearch(query) {
+    var url = 'object?text=' + query;
+    loadDataFrom(url)
+}
+
+$(document).ready(function () {
+    var searchQuery = 'aboriginal';
+    loadDataForSearch(searchQuery);
 });
-
-
-
