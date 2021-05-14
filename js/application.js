2833bcd5b2a4cb43d58e8a1d4c0213d4d4ac4bc8
AnitaHeleta <53992296+AnitaHeleta@users.noreply.github.com>
2021-05-14 12:55:50 +0200

    On the Map section

--- js/application.js
+++ js/application.js
@@ -189,5 +189,19 @@ $(function () {
     $(document).on("click", ".readMore,.readLess", function () {
         $(this).closest(".addReadMore").toggleClass("showlesscontent showmorecontent");
     });
+
+    var mymap = L.map('mapid').setView([-26.289033, 134.605947], 5)
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiaGFuaXRhIiwiYSI6ImNrb282cGtlbzAxdmwydW9qeTRlYmF6aHIifQ.tgxopbicQoMcjq0E1BXHiQ'
+}).addTo(mymap);
+
+
+
 });
 
